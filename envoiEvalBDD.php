53d3e96ed5dca7fef5f752e2711cf506bafe2bdf
AuerBenjamin <benjamin.auer@edu.ece.fr>
2023-05-16 18:29:41 +0200

    correction competences

--- envoiEvalBDD.php
+++ envoiEvalBDD.php
@@ -2,7 +2,21 @@
  session_start();
 
  include("connexionBDD.php");
- $eval = $_POST["eval"];
+
+ $eval = isset($_POST["eval"]) ? $_POST["eval"] : "";
+ $matiere = isset($_POST["matiere"]) ? $_POST["matiere"] : "";
+
+$erreur = "";
+if ($eval == "") {
+    $erreur .= "L'évaluation n'a pas pu être sélectionnée";
+}
+
+if ($erreur == "") {
+    echo "";
+} else {
+    echo "Erreur: <br>" . $erreur;
+}
+
 
  $requete = $bdd->prepare("SELECT * from competences where idCompetence = :idCompetence")
  if ($eval == 1) {

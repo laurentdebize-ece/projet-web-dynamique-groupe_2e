064e84743d3b581893d4af5ed62f2701c7b2dc75
AuerBenjamin <benjamin.auer@edu.ece.fr>
2023-05-17 12:56:31 +0200

    competences qui marchent bof

--- envoiEvalBDD.php
+++ envoiEvalBDD.php
@@ -1,10 +1,21 @@
 <?php
  session_start();
 
- include("connexionBDD.php");
-
+ try
+ {
+ $bdd = new PDO('mysql:host=localhost;dbname=bdece;
+ charset=utf8', 'root', '',
+ array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+ $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ }
+ catch (Exception $e)
+ {
+ die('Erreur : ' . $e->getMessage());
+ }
+ 
  $eval = isset($_POST["eval"]) ? $_POST["eval"] : "";
  $matiere = isset($_POST["matiere"]) ? $_POST["matiere"] : "";
+ $competence = isset($_POST["competence_id"]) ? $_POST["competence_id"] : "";
 
 $erreur = "";
 if ($eval == "") {
@@ -18,9 +29,11 @@ if ($erreur == "") {
 }
 
 
- $requete = $bdd->prepare("SELECT * from competences where idCompetence = :idCompetence")
- if ($eval == 1) {
-
- }
+$sql = "UPDATE competences SET idEval = :nouvelleValeur WHERE idMatiere = :idMatiere AND idCompetence = :idCompetence";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':nouvelleValeur', $eval);
+    $stmt->bindParam(':idMatiere', $matiere);
+    $stmt->bindParam(':idCompetence', $competence);
+ 
 
 ?>

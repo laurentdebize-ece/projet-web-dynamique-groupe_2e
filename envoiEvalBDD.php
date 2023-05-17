79c01978e309f7cc9ab71aec453f7e0330ca278d
AuerBenjamin <benjamin.auer@edu.ece.fr>
2023-05-17 13:22:51 +0200

    c bon ca marche (enfin)

--- envoiEvalBDD.php
+++ envoiEvalBDD.php
@@ -1,21 +1,16 @@
 <?php
- session_start();
+session_start();
 
- try
- {
- $bdd = new PDO('mysql:host=localhost;dbname=bdece;
- charset=utf8', 'root', '',
- array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
- $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- }
- catch (Exception $e)
- {
- die('Erreur : ' . $e->getMessage());
- }
- 
- $eval = isset($_POST["eval"]) ? $_POST["eval"] : "";
- $matiere = isset($_POST["matiere"]) ? $_POST["matiere"] : "";
- $competence = isset($_POST["competence_id"]) ? $_POST["competence_id"] : "";
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=bdece;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+$eval = isset($_POST["eval"]) ? $_POST["eval"] : "";
+$matiere = isset($_POST["matiere"]) ? $_POST["matiere"] : "";
+$competence = isset($_POST["competence_id"]) ? $_POST["competence_id"] : "";
 
 $erreur = "";
 if ($eval == "") {
@@ -28,12 +23,15 @@ if ($erreur == "") {
     echo "Erreur: <br>" . $erreur;
 }
 
-
 $sql = "UPDATE competences SET idEval = :nouvelleValeur WHERE idMatiere = :idMatiere AND idCompetence = :idCompetence";
-    $stmt = $bdd->prepare($sql);
-    $stmt->bindParam(':nouvelleValeur', $eval);
-    $stmt->bindParam(':idMatiere', $matiere);
-    $stmt->bindParam(':idCompetence', $competence);
- 
+$stmt = $bdd->prepare($sql);
+$stmt->bindParam(':nouvelleValeur', $eval);
+$stmt->bindParam(':idMatiere', $matiere);
+$stmt->bindParam(':idCompetence', $competence);
 
+if ($stmt->execute()) {
+    echo "La valeur d'évaluation a été mise à jour avec succès.";
+} else {
+    echo "Erreur lors de la mise à jour de la valeur d'évaluation.";
+}
 ?>

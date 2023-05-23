9a08232f6c36c03d5a150b077773a242c8e20117
Amadou Kassim <kassim.amadou@edu.ece.fr>
2023-05-23 20:26:08 +0200

    pas important

--- matieres.php
+++ matieres.php
@@ -1,89 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style_menu.css">
     <link rel="stylesheet" type="text/css" href="style_footer.css">
-
-
     <title>Matieres</title>
-    <link rel=" stylesheet " href="matieres.css">
-
+    <link rel="stylesheet" href="matieres.css">
 </head>
-
 <body>
     <?php
     include 'menu.php';
-    include("matieresBDD.php");
-
+    include 'connexionBDD.php';
+    session_start();
+    $utilisateur = $_SESSION['utilisateurs'];
+    $sql = "SELECT matieres.idMatiere, matieres.nom
+    FROM matieres
+    JOIN enseignement ON matieres.idMatiere = enseignement.idMatiere
+    JOIN etudiants ON enseignement.idClasse = etudiants.idClasse
+    WHERE etudiants.idEtudiant = :idUtilisateur";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':idUtilisateur', $utilisateur['idUtilisateur']);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
-
-
     <div id="scrollcontainer">
         <div id="containertrue">
-        <form action="evaluer.php" method="POST">
-            <input type="hidden" name="matiere" value="1">
-            <button type="submit" class="Matiere">
-                <p> Mathématiques</p>
-            </button>
-        </form>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 2</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 3</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 4</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 5</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 6</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 7</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 8</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 9</p>
-                </div>
-            </a>
-            <a href="competences.php">
-                <div class="Matiere">
-                    <p> Matière 10</p>
-                </div>
+            <form action="evaluer.php" method="POST">
+                <?php foreach ($result as $r) : ?>
+                    <button type="submit" class="Matiere" id="<?php echo $r['idMatiere']; ?>">
+                        <p><?php echo $r['nom']; ?></p>
+                    </button>
+                <?php endforeach; ?>
+            </form>
         </div>
-
     </div>
-
-
-
+    <?php include 'footer.php'; ?>
 </body>
-<?php
-include 'footer.php';
-?>
 </html>

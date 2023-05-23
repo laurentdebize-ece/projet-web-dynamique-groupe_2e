2fc4728fa920cec1d477625435eb273216aec1b7
Virgile Fievet <virgile.fiev@edu.ece.fr>
2023-05-23 23:27:24 +0200

    ajout possibilité de s'ajouter une compétence

--- toutes competencesBDD.php
+++ toutes competencesBDD.php
@@ -3,31 +3,28 @@ session_start();
 
 include("connexionBDD.php");
 
+$idUtilisateur = isset($_SESSION['utilisateurs']['idUtilisateur']) ? $_SESSION['utilisateurs']['idUtilisateur'] : null;
 
-// Récupérer toutes les matières 
-$sql = $bdd->prepare("SELECT nomCompetence FROM Competences"); 
+$sql = $bdd->prepare("SELECT DISTINCT c.nomCompetence, m.idMatiere FROM Competences c JOIN Matieres m ON c.idMatiere = m.idMatiere"); 
 $sql->execute();
 $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-
 
 
 echo "<ul>";
 
 foreach ($result as $row) {
     $competence_nom = $row['nomCompetence'];
-
+    $matiere_id = $row['idMatiere'];
     echo "<li>$competence_nom ";
-    //echo "<form action='envoiEvalBDD.php' method='POST'>";
-    // changer la ligne précedent pour pouvoir selectionner la matière à ajouter 
 
-    
+    echo "<form action='toutes_competencesBDD.php' method='POST'>";
+    echo "<input type='hidden' name='competence' value='$competence_nom'>";
+    echo "<input type='hidden' name='idMatiere' value='$matiere_id'>"; 
+    echo "<input type='hidden' name='idUtilisateur' value='$idUtilisateur'>";
 
     echo "<input type='submit' value='Enregistrer'>";
     echo "</form></li>";
 }
 
-
 echo "</ul>";
-
-
 ?>
